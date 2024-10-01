926cb555d754a0d3398bf70b5c7fe07883774fea
Matteo AUGER <matteo.auger@protonmail.com>
2024-10-01 19:42:58 +0200

    chore: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { invoke } from '@tauri-apps/api/core'
 import './App.css'
+import './index.css'
 
 function App() {
     const [greetMsg, setGreetMsg] = useState('')
